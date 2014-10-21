0495057fe200c274083ee49095d79dd62cc2756b
Geoff Holmes <geoffrholmes@gmail.com>
2014-10-21 15:30:18 +0100

    correct getNode

--- @grhTree/getNode.m
+++ @grhTree/getNode.m
@@ -3,13 +3,18 @@ function thisNode = getNode(obj, testpoint)
 % find the node / leaf to which a testpoint belongs
 
 thisNode = obj;
+cc = 0;
 
-while ~isempty(thisNode.splitVar)
+while ~isempty(thisNode.Rchild) && cc < 5
+    
+    cc = cc + 1
+    
+    thisNode
     
     if testpoint(thisNode.splitVar) > thisNode.splitVal
-        thisNode = obj.Rchild;
+        thisNode = thisNode.Rchild;
     else
-        thisNode = obj.Lchild;
+        thisNode = thisNode.Lchild;
     end
     
 end
