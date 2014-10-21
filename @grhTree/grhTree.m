040cbbdf8e3c5768bca3da88c5f012fddbb2650f
Geoff Holmes <geoffrholmes@gmail.com>
2014-10-21 13:39:45 +0100

    make tree subclass of node

--- @grhTree/grhTree.m
+++ @grhTree/grhTree.m
@@ -1,16 +1,16 @@
-classdef grhTree < handle
+classdef grhTree < grhNode
     
     properties
         
-        data;        % data inputs
+%         data;        % data inputs
         input_dim;   % dimension of datapoint inputs
         Ndata;       % total number of datapoints
         data_range;  % extent of data space
-        depth = 1;   % depth of tree
-        splitVar;    %
-        splitVal     %
-        Lchild;      %
-        Rchild;
+%         depth = 1;   % depth of tree
+%         splitVar;    %
+%         splitVal     %
+%         Lchild;      %
+%         Rchild;
         
     end
     
@@ -21,20 +21,23 @@ classdef grhTree < handle
             % initial contructor of tree
             if nargin > 0
                 if isstruct(data1)
-                    obj.data = [data1.x data1.y];
-                    obj.input_dim = size(data1.x, 2);
+                    data = [data1.x data1.y];
+                    input_dim = size(data1.x, 2);
                 else
                     if nargin == 2 
-                        obj.data = [data1 data2];
-                        obj.input_dim = size(obj.data, 2);
+                        data = [data1 data2];
+                        input_dim = size(data, 2);
                     else
                         % assume final colunn is output
-                        obj.data = data1;
-                        obj.input_dim = size(obj.data, 2) - 1;
+                        data = data1;
+                        input_dim = size(data, 2) - 1;
                     end
                 end
             end
             
+            parent = struct('tree', struct('depth', 0), 'depth', 0);
+            obj = obj@grhNode(parent, data);
+            obj.input_dim = input_dim;
             obj.Ndata = size(obj.data, 1);
             obj.data_range = [min(obj.data); max(obj.data);];
 %             obj.trunk  = grhNode(obj);
