060e571c79a27f3843e77c99f2a994c4b7b61cf4
Geoff Holmes <geoffrholmes@gmail.com>
2014-10-24 08:40:25 +0100

    re-order and comments

--- @grhTree/drawTree.m
+++ @grhTree/drawTree.m
@@ -1,20 +1,24 @@
 function handle = drawTree(obj, opts)
 
+% plot tree by calling recursive node plotting function
 
+% if tree has no branches don't plot
 if obj.total_depth == 1
     msgbox('Skipped drawing of unbranched tree', 'treePlot', 'warn')
     return
 end
-% handle to return
-handle = figure; hold on; axis off;
-% starting point at base of tree
-refPoint = [0 0];
+
 % set plotting parameters if not specified in function call
 if nargin == 1
     opts = struct('d1', 0.5, 'd2', 0.75);
     % d1 - length of vertical drop lines
     % d2 - distance of drop to next starting point (refPoint)
 end
+
+% handle to return
+handle = figure; hold on; axis off;
+% starting point at base of tree
+refPoint = [0 0];
 
 % call function that recursively draws each node
 limits = obj.nodes(1).drawNode(refPoint, opts);
