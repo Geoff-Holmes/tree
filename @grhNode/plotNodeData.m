05c72c603460649a04c40294b0fa540dec7aa5f0
Geoff Holmes <geoffrholmes@gmail.com>
2014-10-24 13:59:47 +0100

    add indiv node data plotter

---
+++ @grhNode/plotNodeData.m
@@ -0,0 +1,9 @@
+function handle = plotNodeData(obj)
+
+data = obj.data;
+d1 = sum(data(:,1) == data(1,1));
+d2 = sum(data(:,2) == data(1,2));
+x  = reshape(data(:,1), d1, d2);
+y  = reshape(data(:,2), d1, d2);
+z  = reshape(data(:,3), d1, d2);
+handle = surf(x,y,z)
