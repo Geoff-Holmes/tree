6a1c280df35a533647137a9d012c223054d40316
Geoff Holmes <geoffrholmes@gmail.com>
2014-10-24 08:30:48 +0100

    move plot optscheck to top level

--- @grhNode/drawNode.m
+++ @grhNode/drawNode.m
@@ -3,14 +3,6 @@ function limits = drawNode(obj, zeroPoint, opts)
 % recursive function called by drawTree.m for graphical output of entire
 % tree structure
 
-if nargin < 3
-    
-    opts = struct('d1', 0.25, 'd2', 0.5, 'd3', 0.75);
-    % d1 - not used
-    % d2 - length of vertical drop lines
-    % d3 - distance to drop to next starting point (refPoint)
-end
-
 % output the split criteria inequality <= on left > on right
 text(zeroPoint(1), zeroPoint(2), sprintf('x%d <> %.3f', obj.splitVar, obj.splitVal), 'horizontalAlignment', 'center', 'verticalAlignment', 'bottom')
 
@@ -19,33 +11,33 @@ x = zeroPoint(1)+[-1;1]*1/obj.depth;
 y = zeroPoint(2)*[1;1];
 line(x, y)
 % draw horizontal lines to next leaf / node
-line(x(1)*[1;1], [y(1); y(1)-opts.d2])
-line(x(2)*[1;1], [y(1); y(1)-opts.d2])
+line(x(1)*[1;1], [y(1); y(1)-opts.d1])
+line(x(2)*[1;1], [y(1); y(1)-opts.d1])
 
 % test for continuance of left branch
 if ~isempty(obj.Lchild.splitVar)
     % draw subnode
-    limits = drawNode(obj.Lchild, [x(1) y(1)-opts.d3], opts);
+    limits = drawNode(obj.Lchild, [x(1) y(1)-opts.d2], opts);
     leftLim = limits(1);
     bottomLimL = limits(3);
 else
     % write number of data elements at leaf
     leftLim = x(1);
-    bottomLimL = y(1)-opts.d3;
-    text(leftLim, y(1)-opts.d3, sprintf('%d', size(obj.Lchild.data, 1)), 'horizontalAlignment', 'center', 'verticalAlignment', 'bottom');
+    bottomLimL = y(1)-opts.d2;
+    text(leftLim, y(1)-opts.d2, sprintf('%d', size(obj.Lchild.data, 1)), 'horizontalAlignment', 'center', 'verticalAlignment', 'bottom');
 end
 
 % test for continuance of right branch
 if ~isempty(obj.Rchild.splitVar)
     % draw subnode
-    limits = drawNode(obj.Rchild, [x(2) y(1)-opts.d3], opts);
+    limits = drawNode(obj.Rchild, [x(2) y(1)-opts.d2], opts);
     rightLim = limits(2);
     bottomLimR = limits(3);
 else
     % write number of data elements at leaf
     rightLim = x(2);
-    bottomLimR = y(1)-opts.d3;
-    text(rightLim, y(1)-opts.d3, sprintf('%d', size(obj.Rchild.data, 1)), 'horizontalAlignment', 'center', 'verticalAlignment', 'bottom');
+    bottomLimR = y(1)-opts.d2;
+    text(rightLim, y(1)-opts.d2, sprintf('%d', size(obj.Rchild.data, 1)), 'horizontalAlignment', 'center', 'verticalAlignment', 'bottom');
 end
 
 % update limits of graphical output
--- @grhTree/drawTree.m
+++ @grhTree/drawTree.m
@@ -4,12 +4,15 @@ function handle = drawTree(obj, opts)
 handle = figure; hold on; axis off;
 % starting point at base of tree
 refPoint = [0 0];
-% call function that recursively draws each node
+% set plotting parameters if not specified in function call
 if nargin == 1
-    limits = obj.nodes(1).drawNode(refPoint);
-else
-    limits = obj.nodes(1).drawNode(refPoint, opts);
+    opts = struct('d1', 0.5, 'd2', 0.75);
+    % d1 - length of vertical drop lines
+    % d2 - distance of drop to next starting point (refPoint)
 end
+
+% call function that recursively draws each node
+limits = obj.nodes(1).drawNode(refPoint, opts);
 
 % ensure whole graphic is visible
 axis([limits * 1.1 0.25]);
