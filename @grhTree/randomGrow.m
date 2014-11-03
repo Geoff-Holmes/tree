a839e7cb4d73775911b7ae912c6000843ba0b002
Geoff Holmes <geoffrholmes@gmail.com>
2014-11-03 11:41:47 +0000

    allow for now possible split and try each leaf once only

--- @grhTree/randomGrow.m
+++ @grhTree/randomGrow.m
@@ -2,12 +2,17 @@ function obj = randomGrow(obj)
 
 % random grow operation see gramacy_bayesian_2008 p1124
 
+randLeafInds = randperm(length(obj.leaves));
 flag = 0;
-while ~flag
+counter = 0;
+while ~flag && counter < length(randLeafInds)
     % choose randomly from all leaves
-    leaf = obj.nodes(obj.leaves(randi(length(obj.leaves))));
+    counter = counter + 1;
+    leaf = obj.nodes(obj.leaves(randLeafInds(counter)));
     flag = size(leaf.data, 1) - 1;
 end
+if ~flag, fprintf('\nData already fully split.\n'), return, end
+
 % choose randomly from all dimensions
 dim = randi(obj.input_dim);
 % choose randomly from all possible split point on this leaf / dim
