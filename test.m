42f0bce864ad0cc9f71112c8c2f9db7484e1aac9
Geoff Holmes <geoffrholmes@gmail.com>
2014-11-06 19:59:26 +0000

    minor except temp fix for total_depth

--- test.m
+++ test.m
@@ -1,25 +1,67 @@
 % test grow and prune
 
-t = grhTree(rand(3,33)')
+clear all
+close all
+p = [2 1 1 1];
+p = cumsum(p);
+p = p/max(p);
+flag = 0;
+
+t = grhTree(rand(333,2))
 for i = 1:5
     t.randomGrow;
 end
-for i = 1:100
-if rand < .5
+tLast = [];
+while true
+if rand < p(1)
+    fprintf('\nGrowing:')
     t.randomGrow;
-    fprintf('\nGrow\n')
+%     fprintf('\n')    
 else
-    if rand < .5
+    if rand < p(2)
+        fprintf('\nPruning:')
         t.randomPrune;
-        fprintf('\nPrune\n')
+%         fprintf('\n')  
     else
-        t.randomChange;
-        fprintf('\nChange\n')
+        if rand < p(3)
+            fprintf('\nSwapping:')
+            t.randomSwap;
+%             fprintf('\n')
+        else
+            fprintf('\nChanging:')
+            t.randomChange;
+%             fprintf('\n')    
+        end
     end
 end
-pause() 
+% pause() 
 % close all
 t.drawTree;
-% title(sprintf('depth %d', t.total_depth))
-% pause()
+
+t.total_depth = max([t.nodes.depth]);
+
+t.testDepthConsistency;
+if isfield(t.health, 'depthFail'), break;
+% else fprintf('\npassed depth\n')
+end
+
+t.testAllDataAtLeaves;
+if isfield(t.health, 'lostData'), break;
+end
+
+t.testDataAllLengths;
+if isfield(t.health, 'allDataLengths'), break;
+end
+
+t.testDataPlacement('all');
+if isfield(t.health, 'misplacedData'), break;
+end
+
+t.testParentChildConsistency;
+if isfield(t.health, 'relationError'), break;
+end
+
+if ~isempty(fields(t.health))
+    t.health
+end
 end
