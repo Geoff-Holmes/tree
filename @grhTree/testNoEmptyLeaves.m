d4962f960be23cd25ead9dc6fa8ba6b52261be9a
Geoff Holmes <geoffrholmes@gmail.com>
2014-11-11 15:16:51 +0000

    new test function

---
+++ @grhTree/testNoEmptyLeaves.m
@@ -0,0 +1,11 @@
+function obj = testNoEmptyLeaves(obj)
+
+% test for no leaves empty of data
+for leaf = obj.leaves
+    try
+        assert(logical(numel(leaf.dataIDs)))
+    catch
+        obj.health.emptyLeaf = 1;
+        fprintf('\nNo data at leaf %d\n', leaf.ID)
+    end
+end
