349e2ebfdace23af5f62bd716c3ef80f8a2fe099
Geoff Holmes <geoffrholmes@gmail.com>
2014-11-13 15:54:06 +0000

    This version correct ? No gaurentee chosen leaf will grow depth is much more constrained - depending on choice of tree.prior.a / b

--- @grhTree/randomGrow.m
+++ @grhTree/randomGrow.m
@@ -1,35 +1,18 @@
 function obj = randomGrow(obj)
 
 % random grow operation see gramacy_bayesian_2008 p1124
-% grow prob depends on depth of leaf
 
-% get exact and approx growth probs for each leaf
-growProbs  = obj.prior.a * double([obj.leaves.depth]) .^ -obj.prior.b;
-% get approximate growth probs
-growProbs  = round(10 * growProbs / min(growProbs));
-% prepare for sampling
-F = 1:max(growProbs);
-I = meshgrid(growProbs, F);
-[X, Y] = meshgrid(1:numel(obj.leaves), F);
-randLeafInds = X(Y<=I);
-
+leaf = obj.leaves(randi(length(obj.leaves)));
 % randLeafInds = randperm(length(obj.leaves));
-flag = 0;
+% flag = 0;
 % counter = 0;
-while ~flag %&& counter < length(randLeafInds)
-    p = cumsum(1:numel(randLeafInds));
-    p = p / p(end);
+% while ~flag && counter < length(randLeafInds)
     % choose randomly from all leaves
 %     counter = counter + 1;
-    ind = find(rand <= p, 1, 'first');
-    leaf = obj.leaves(randLeafInds(ind));
+%     leaf = obj.leaves(randLeafInds(counter));
     % discard leaf if only one datapoint
-    if numel(leaf.dataIDs) == 1
-        randLeafInds = randLeafInds(randLeafInds ~= randLeafInds(ind));
-    else
-        flag = 1;
-    end
-end
+    flag = numel(leaf.dataIDs) - 1 && rand <= obj.prior.a * double(leaf.depth) ^ -obj.prior.b;
+% end
 if ~flag
     % fprintf('\nData already fully split.\n')
     return
@@ -51,3 +34,5 @@ splitPoint = midpoints(randi(length(midpoints)));
 leaf.splitNode(dim, splitPoint);
 
 % fprintf('\nGrowing from node %d, new leaves %d - %d\n', leaf.ID, leaf.Lchild.ID, leaf.Rchild.ID)
+
+% model management to be added
