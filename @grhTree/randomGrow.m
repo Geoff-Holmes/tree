5ec01069d8b17735d0b849debe04413e07194848
Geoff Holmes <geoffrholmes@gmail.com>
2014-11-13 15:24:39 +0000

    weight probs by depth must grow unless all leaves already fully split This is not how it should work - ie. it should be possible to not split

--- @grhTree/randomGrow.m
+++ @grhTree/randomGrow.m
@@ -1,16 +1,34 @@
 function obj = randomGrow(obj)
 
 % random grow operation see gramacy_bayesian_2008 p1124
+% grow prob depends on depth of leaf
 
-randLeafInds = randperm(length(obj.leaves));
+% get exact and approx growth probs for each leaf
+growProbs  = obj.prior.a * double([obj.leaves.depth]) .^ -obj.prior.b;
+% get approximate growth probs
+growProbs  = round(10 * growProbs / min(growProbs));
+% prepare for sampling
+F = 1:max(growProbs);
+I = meshgrid(growProbs, F);
+[X, Y] = meshgrid(1:numel(obj.leaves), F);
+randLeafInds = X(Y<=I);
+
+% randLeafInds = randperm(length(obj.leaves));
 flag = 0;
-counter = 0;
-while ~flag && counter < length(randLeafInds)
+% counter = 0;
+while ~flag %&& counter < length(randLeafInds)
+    p = cumsum(1:numel(randLeafInds));
+    p = p / p(end);
     % choose randomly from all leaves
-    counter = counter + 1;
-    leaf = obj.leaves(randLeafInds(counter));
+%     counter = counter + 1;
+    ind = find(rand <= p, 1, 'first');
+    leaf = obj.leaves(randLeafInds(ind));
     % discard leaf if only one datapoint
-    flag = numel(leaf.dataIDs) - 1;
+    if numel(leaf.dataIDs) == 1
+        randLeafInds = randLeafInds(randLeafInds ~= randLeafInds(ind));
+    else
+        flag = 1;
+    end
 end
 if ~flag
     % fprintf('\nData already fully split.\n')
@@ -33,5 +51,3 @@ splitPoint = midpoints(randi(length(midpoints)));
 leaf.splitNode(dim, splitPoint);
 
 % fprintf('\nGrowing from node %d, new leaves %d - %d\n', leaf.ID, leaf.Lchild.ID, leaf.Rchild.ID)
-
-% model management to be added
