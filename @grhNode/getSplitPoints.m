8c5639c98d3b88643ccd4d0e775b47d533543c60
Geoff Holmes <geoffrholmes@gmail.com>
2014-11-17 10:21:02 +0000

    remove costly unique

--- @grhNode/getSplitPoints.m
+++ @grhNode/getSplitPoints.m
@@ -3,6 +3,13 @@ function [splitPoints, dataIDs] = getSplitPoints(obj, dim)
 % get candidate splitpoints (midpoints) along dimension dim of the data for
 % this node
 
+% get data
 dataIDs = obj.pullDataDown;
-temp = sort(unique(obj.data.getInputs(dataIDs, dim)));
+temp = sort(obj.data.getInputs(dataIDs, dim));
+
+% weed out repeated data
+inds = logical((temp(2:end) - temp(1:end-1)));
+temp = temp([inds; true]);
+
+% calculate midpoints
 splitPoints = (temp(1:end-1) + temp(2:end)) / 2;
