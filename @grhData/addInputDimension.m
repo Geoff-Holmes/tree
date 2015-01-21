6b9d455915a5cadee60f9bb660333c6767e42d71
Geoff Holmes <geoffrholmes@gmail.com>
2015-01-21 12:48:34 +0000

    new function

---
+++ @grhData/addInputDimension.m
@@ -0,0 +1,5 @@
+function obj = addInputDimension(obj, newColumn)
+
+obj.data      = [obj.data(1:obj.input_dim) newColumn obj.data(obj.input_dim+1:end)];
+obj.input_dim = obj.input_dim + 1;
+obj.range     = [obj.range(1:obj.input_dim) [min(newColumn); max(newColumn)] obj.data(obj.input_dim+1:end)];
